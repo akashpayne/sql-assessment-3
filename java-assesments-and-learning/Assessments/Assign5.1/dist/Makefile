0e78bc086dea952da72464b2754ad41ea07ca5dd
Akash Payne <akashpayne@gmail.com>
2017-07-31 11:57:36 +0100

    Added files from old git

--- java-assesments-and-learning/Assessments/Assign5.1/dist/Makefile
+++
@@ -1,8 +0,0 @@
-
-DiskSim.class: DiskSim.java BRequest.java DSched.java
-	@javac DiskSim.java BRequest.java DSched.java
-
-.PHONY: clean
-clean:
-	@rm -f *.class
-
--- java-assesments-and-learning/Test-employment-2/Main.java
+++
@@ -1,9 +0,0 @@
-// the main (runnable) class
-public class Main {
-
-	// runs the login class initially
-	public static void main(String[] args) {
-		DBConnect db = new DBConnect();
-		new Frame("Login", db);
-	}
-}
